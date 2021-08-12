b7eaa524719ac75bda3c844bb4b1103691272790
leminhduc1202 <duc.leminh.dn@gmail.com>
2021-08-12 21:52:04 +0700

    up code

---
+++ app/src/main/java/mdideas/devapp/goldprice/MainActivity.kt
@@ -0,0 +1,38 @@
+package mdideas.devapp.goldprice
+
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mWebView = findViewById<View>(R.id.webview) as WebView
+        mWebView.loadUrl("https://goldprice.org/")
+
+        val webSettings = mWebView.settings
+        webSettings.javaScriptEnabled = true
+        mWebView.webViewClient = WebViewClient()
+
+        mWebView.canGoBack()
+        mWebView.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_BACK
+
+                && event.action == MotionEvent.ACTION_UP
+                && mWebView.canGoBack()) {
+                mWebView.goBack()
+                return@OnKeyListener true
+            }
+            false
+        })
+
+    }
+}
